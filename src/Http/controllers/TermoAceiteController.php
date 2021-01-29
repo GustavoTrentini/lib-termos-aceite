7d78382b60b787ac01febcb95a56e890736a31e3
Gustavo Trentini <gustavo.trentini@applysystem.com.br>
2021-01-29 18:53:08 -0300

    Correção de input checkbox e correção de aceite de termo

--- src/Http/controllers/TermoAceiteController.php
+++ src/Http/controllers/TermoAceiteController.php
@@ -57,8 +57,8 @@ class TermoAceiteController extends Controller
                 ]);
         }
         else{
-            //deslogar usuário
-            return "usuário deslogado";
+            Auth::logout();
+            return redirect('/');
         }
     }
 
