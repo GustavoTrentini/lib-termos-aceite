3e8c860a4d7453971b33d992b33af95f4ed995ce
Gustavo Trentini <gustavo.trentini@applysystem.com.br>
2021-01-29 13:03:23 -0300

    Correção de caminho de rotas do pacote

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,14 +1,10 @@
 <?php
-
-namespace ApplySystem\TermosAceite\Routes;
-
+use Illuminate\Support\Facades\Route;
 use ApplySystem\TermosAceite\Http\Controllers\TermoAceiteController;
-use Illuminate\Routing\Route;
 
 Route::group(['middleware' => ['web','auth']], function(){
     Route::get('termo/aceite', [TermoAceiteController::class, 'index'])->name('termo.aceite');
     Route::get('termo/create', [TermoAceiteController::class, 'create'])->name('termo.create');
     Route::post('termo/store', [TermoAceiteController::class, 'store'])->name('termo.store');
-
 });
 
